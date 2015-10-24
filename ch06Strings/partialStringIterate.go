605d1fec7bc581f2949d2f2d98331dfd381a2703
Peng Zhai <pengzhai@uber.com>
2015-10-24 12:18:33 -0400

    Trim and split strings.

---
+++ ch06Strings/partialStringIterate.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"unicode/utf8"
+	"fmt"
+)
+
+func main() {
+	str := "Étoilé"
+	rune := make([]byte, 0, 4)
+	for i := 0; i < len(str); i++ {
+		rune = append(rune, str[i])
+		if (utf8.FullRune(rune)) {
+			char, _ := utf8.DecodeRune(rune)
+			fmt.Printf("%c", char)
+			rune = rune[0:0]
+		}
+	}
+	fmt.Printf("\n")
+}
---
+++ ch06Strings/trimStrings.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"strings"
+	"fmt"
+)
+
+func main() {
+	str := "\tThis is a string \n"
+	str = strings.Trim(str, " \t\n\r")
+	words := strings.Split(str, " ")
+	for _, word := range words {
+		fmt.Printf("%s\n", word)
+	}
+}
