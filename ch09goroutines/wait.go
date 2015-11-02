ed056d5e2cbc49e8f5df384433305a73fc26c4b7
Peng Zhai <pengzhai@uber.com>
2015-11-01 19:32:08 -0500

    Use wait.

---
+++ ch09goroutines/wait.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"sync"
+	"os"
+	"strings"
+	"fmt"
+)
+
+func main() {
+	var w sync.WaitGroup
+	for _, v := range os.Args {
+		w.Add(1)
+		go func(str string) {
+			fmt.Printf("%s\n", strings.ToUpper(str))
+			w.Done()
+		}(v)
+	}
+	w.Wait()
+}
