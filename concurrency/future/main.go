3308c482bc055a7c39161dbee4619cd0cc557316
Peng Zhai <pengzhai@uber.com>
2015-11-07 22:29:16 -0500

    Implement naive map reduce.

--- concurrency/future/main.go
+++ concurrency/future/main.go
@@ -1,25 +1,28 @@
 package main
+
 import (
-	"sync"
-	"strconv"
 	"fmt"
+	"strconv"
+	"sync"
 )
 
 type futureInt64 struct {
-	ch chan int64
-	v int64
+	ch      chan int64
+	v       int64
 	collect sync.Once
 }
 
 func (f *futureInt64) String() string {
-	f.collect.Do(func() {f.v = <- f.ch})
+	f.collect.Do(func() { f.v = <-f.ch })
 	return strconv.FormatInt(f.v, 10)
 }
 
 func fib(n int64) (int64, int64) {
-	if n < 2 {return 1,1}
-	f1, f2 := fib(n-1)
-	return f2, f1+f2
+	if n < 2 {
+		return 1, 1
+	}
+	f1, f2 := fib(n - 1)
+	return f2, f1 + f2
 }
 
 func Fib(n int64) fmt.Stringer {
