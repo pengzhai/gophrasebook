811e1932443b2d4a11e559d4dd2873cd63e86e50
Peng Zhai <pengzhai@uber.com>
2015-11-08 00:42:33 -0500

    Try timer.

---
+++ datetimes/timer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	time.AfterFunc(2000000000, func() {
+		fmt.Printf("Timer expired\n")
+	})
+	timer := time.NewTimer(3000000000)
+	time := <-timer.C
+	fmt.Printf("Current time: %d nanoseconds\n", time.UnixNano())
+}
