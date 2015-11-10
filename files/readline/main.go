3c49cc2600f0043191ce92bb976dc971f16e2cf4
Peng Zhai <pengzhai@uber.com>
2015-11-09 23:08:05 -0500

    Read fiels.

---
+++ files/readline/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("lineRead.go")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return
+	}
+	lineNumber := 1
+	lineReader := bufio.NewReaderSize(file, 20)
+	for line, isPrefix, e := lineReader.ReadLine(); e == nil; line, isPrefix, e = lineReader.ReadLine() {
+		fmt.Printf("%.3d: ", lineNumber)
+		lineNumber++
+		os.Stdout.Write(line)
+		if isPrefix {
+			for {
+				line, isPrefix, _ = lineReader.ReadLine()
+				os.Stdout.Write(line)
+				if !isPrefix {
+					break
+				}
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
